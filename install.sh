e34dfd6a6d2f1a80f410a875104263e07169e087
Ruchi Niwalkar <niwalkar.r@husky.neu.edu>
2020-03-10 13:48:39 -0400

    adding code deploy agent in Ami

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get install apache2 -y
 sudo apt-get install -y openjdk-11-jdk
 java -version
+sudo apt-get install ruby -y
+sudo apt-get install wget -y
+cd /home/ubuntu
+wget https://${BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
