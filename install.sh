0ba917f73bdf38d10c07c209a9c6e62f19459989
Ruchi Niwalkar <niwalkar.r@husky.neu.edu>
2020-03-18 00:06:50 -0400

    adding cloud watch agent

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
 sudo service codedeploy-agent status
+wget https://s3."${AWS_REGION}".amazonaws.com/amazoncloudwatch-agent-"${AWS_REGION}"/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
